b40e0bbfd94a70bed99a445a980c856a27c865f3
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2021-10-09 15:15:06 +0200

    ejecting

---
+++ src/app/components/Layouts/Basic/index.tsx
@@ -0,0 +1,8 @@
+import React, {FunctionComponent} from "react";
+import {BasicLayoutProps} from "./types";
+
+const BasicLayout: FunctionComponent<BasicLayoutProps> = (props) => {
+  return <React.Fragment>sss</React.Fragment>;
+};
+
+export default BasicLayout;
