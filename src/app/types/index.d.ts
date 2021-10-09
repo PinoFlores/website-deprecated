35b05094de64f4431735314b84fdb36a45e93efa
Jose Aburto <77801995+PinoFlores@users.noreply.github.com>
2021-10-09 21:15:48 +0200

    fixing

--- src/app/types/index.d.ts
+++ src/app/types/index.d.ts
@@ -1,7 +1,9 @@
+type BreakPointType = "auto" | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | bool;
+
 export type Responsive = {
-  xs?: number;
-  sm?: number;
-  md?: number;
-  lg?: number;
-  xl?: number;
+  xs?: BreakPointType;
+  sm?: BreakPointType;
+  md?: BreakPointType;
+  lg?: BreakPointType;
+  xl?: BreakPointType;
 };
